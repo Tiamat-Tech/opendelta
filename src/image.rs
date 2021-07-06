30ccfc0997568151e4f2c6f6ab4d5bf82ec12849
Nikita Krupitskas <nikita@krupitskas.com>
2021-07-06 15:19:39 +0300

    Properly load and render pcx

--- src/image.rs
+++ src/image.rs
@@ -1,15 +1,78 @@
-struct Image
-{
-    width: u32,
-    height: u32,
-    data: Vec<u32>
+use std::path::Path;
+use std::{io, iter};
+
+// Delta force inner image format
+pub struct DFImage {
+    pub width: u16,
+    pub height: u16,
+    pub data: Vec<u32>,
 }
 
-impl Image {
-    fn load(path: String) -> Result<Image, &'static str>
-    {
-
-        return Err("Can't load image")
+impl DFImage {
+    // Can load PCX, TGA, JPG
+    pub fn load(path: &Path) -> Result<Self, &'static str> {
+        let extension = path.extension();
+        match extension {
+            Some(ext) => match ext.to_str().unwrap_or("").to_lowercase().as_str() {
+                "pcx" => {
+                    return DFImage::load_pcx(path);
+                }
+                "jpg" => {
+                    return DFImage::load_jpg(path);
+                }
+                _ => {
+                    return Err("Not supported extension");
+                }
+            },
+            None => {
+                return Err("Path haven't extension");
+            }
+        }
+        return Err("Can't load image");
     }
-    
+
+    fn load_pcx(path: &Path) -> Result<Self, &'static str> {
+        let mut pcx = pcx::Reader::from_file(path).unwrap();
+
+        let width = pcx.width();
+        let height = pcx.height();
+
+        let mut image = Vec::new();
+
+        if pcx.is_paletted() {
+            for _ in 0..pcx.height() {
+                let mut row: Vec<u8> = iter::repeat(0).take(pcx.width() as usize).collect();
+                pcx.next_row_paletted(&mut row).unwrap();
+                image.push(row);
+            }
+        } else {
+            unimplemented!("Not paletted pcx not yet supported.")
+        }
+
+        let mut palette = [0; 256 * 3];
+        pcx.read_palette(&mut palette).unwrap();
+
+        let mut u32_image: Vec<u32> = Vec::new();
+        for y in 0..height {
+            for x in 0..width {
+                let i = image[y as usize][x as usize] as usize;
+                let pcx_r = palette[i * 3 + 0];
+                let pcx_g = palette[i * 3 + 1];
+                let pcx_b = palette[i * 3 + 2];
+
+                // TODO(optim): Not very effective
+                u32_image.push(((pcx_r as u32) << 16) | ((pcx_g as u32) << 8) | (pcx_b as u32));
+            }
+        }
+
+        return Ok(DFImage {
+            width,
+            height,
+            data: u32_image
+        });
+    }
+
+    fn load_jpg(path: &Path) -> Result<Self, &'static str> {
+        return Err("Can't load image");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -23,41 +23,40 @@ writer.finish().unwrap();
 
  */
 
+pub struct DFTerrain {
+    height_image: image::DFImage,
+}
+
+pub struct Point2D {
+    x: i32,
+    y: i32,
+}
+
+// Voxel
+pub fn render_scene(
+    point: Point2D,
+    height: f32,
+    scale_height: f32,
+    distance: f32,
+    screen_width: u32,
+    screen_height: u32,
+) {
+}
+
 fn main() {
-    let mut reader = pcx::Reader::from_file("data/dfd1/dfd1_d.pcx").unwrap();
-
-    println!(
-        "width = {}, height = {}, paletted = {}",
-        reader.width(),
-        reader.height(),
-        reader.is_paletted()
-    );
-
-    let mut pcx_buf: Vec<u8> = Vec::new();
-
-    for _ in 0..reader.height() {
-        if reader.is_paletted() {
-            let mut row_buf = vec![0; reader.width() as usize];
-            reader.next_row_paletted(&mut row_buf).unwrap();
-            pcx_buf.append(&mut row_buf.clone());
-        } else {
-            // call reader.next_row_rgb(...) or reader.next_row_rgb_separate(...) to read next row
-        }
-    }
-
-    let u32_buffer: Vec<u32> = pcx_buf
-        .chunks_exact(3)
-        .map(|v| ((v[0] as u32) << 16) | ((v[1] as u32) << 8) | v[2] as u32)
-        .collect();
-
-    let width: usize = (reader.width()) as usize;
-    let height: usize = (u32_buffer.len() as f32 / width as f32) as usize;
-    let _buf_size = width * height;
+    // let clouds01 = image::DFImage::load("data/dfd1/CLOUDS01.PCX").unwrap();
+    // let dfd1_c = image::DFImage::load("data/dfd1/DFD1_C.JPG").unwrap();
+    // let dfd1_cm = image::DFImage::load("data/dfd1/DFD1_CM.TGA").unwrap();
+    let dfd1_d = image::DFImage::load(Path::new("data/dfd1/DFD1_D.PCX")).unwrap();
+    // let dfd1_dm = image::DFImage::load("data/dfd1/DFD1_DM.PCX").unwrap();
+    // let dfd1_m = image::DFImage::load("data/dfd1/DFD1_M.PCX").unwrap();
+    // let ripple1 = image::DFImage::load("data/dfd1/RIPPLE1.PCX").unwrap();
+    // let skygrd01 = image::DFImage::load("data/dfd1/SKYGRD01.TGA").unwrap();
 
     let mut window = Window::new(
         "Open Delta",
-        (width as f32 / 3 as f32) as usize,
-        height,
+        dfd1_d.width as usize,
+        dfd1_d.height as usize,
         WindowOptions {
             resize: true,
             scale_mode: ScaleMode::Center,
@@ -72,7 +71,7 @@ fn main() {
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window
-            .update_with_buffer(&u32_buffer, width, height)
+            .update_with_buffer(&dfd1_d.data, dfd1_d.width as usize, dfd1_d.height as usize)
             .unwrap();
     }
 }
